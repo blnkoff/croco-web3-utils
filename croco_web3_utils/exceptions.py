ce8d81ad86e91aa16c8494f0c54262befd1a8ed5
blnkoff <abelenkov2006@gmail.com>
2023-11-08 18:45:51 +0300

    Adding exception InvalidToken and type TokenOrAddress

--- croco_web3_utils/exceptions.py
+++ croco_web3_utils/exceptions.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from ether_wallet.types import Network
 
 
@@ -7,3 +9,10 @@ class ContractNotFound(Exception):
     def __init__(self, defi: str, destination_network: Network, supported_networks: list[Network]):
         super().__init__(f"Contract is not found in {destination_network}. {defi} supports the following networks: "
                          f"{', '.join(supported_networks)}")
+
+
+class InvalidToken(ValueError):
+    """Raised when token is not supported in a specific network in a contract"""
+
+    def __init__(self, token: Any, network: str, defi: str):
+        super().__init__(f"Token {token} is not supported in {network} for using in {defi}")
--- croco_web3_utils/types.py
+++ croco_web3_utils/types.py
@@ -1,6 +1,7 @@
-from typing import Literal
-from ether_wallet.types import ABI
+from typing import Literal, Union
+from ether_wallet.types import ABI, AddressLike
 from web3.contract import AsyncContract, Contract
 
 ContractVersion = Literal[2, 3]
 ContractMap = dict[str, AsyncContract | ABI | Contract]
+TokenOrAddress = Union[AddressLike, str]
