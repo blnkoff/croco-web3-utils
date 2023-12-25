14a19b1433d23762469aefe95fde6d047549e7ef
blnkoff <abelenkov2006@gmail.com>
2023-12-25 15:23:20 +0300

    Changing __init__.py

--- croco_web3_utils/__init__.py
+++ croco_web3_utils/__init__.py
@@ -9,15 +9,6 @@ The package containing utilities to develop Web3-based projects
 
 __version__ = "0.1.0"
 
-__all__ = [
-    'Defi',
-    'load_contracts',
-    'get_deadline',
-    'validate_network',
-    'ContractVersion',
-    'ContractMap'
-]
-
 from .abc import Defi
 from .utils import load_contracts, get_deadline, validate_network
 from .types import ContractMap, ContractVersion, TokenOrAddress
