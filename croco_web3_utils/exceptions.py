efc13202ee6f46da2b96f61e0b4fa5bf8d8780eb
blnkoff <abelenkov2006@gmail.com>
2024-01-24 20:38:11 +0300

    Removing DEXes

--- croco_web3_utils/exceptions.py
+++ croco_web3_utils/exceptions.py
@@ -1,5 +1,4 @@
 from typing import Any
-from evm_wallet.types import AddressLike
 
 
 class ContractNotFound(ValueError):
@@ -23,18 +22,3 @@ class InvalidRoute(ValueError):
     def __init__(self, input_token: str, output_token: str, source_network: str, destination_network: str, defi: str):
         super().__init__(f"Token route {input_token} ({source_network}) --> {output_token} ({destination_network}) is "
                          f"not valid for {defi}")
-
-
-class PoolNotFound(ValueError):
-    """Raised when liquidity pool doesn't exists for particular tokens"""
-
-    def __init__(self, token0: AddressLike, token1: AddressLike):
-        super().__init__(f"0 address returned. Pool does not exist for tokens {token0} and {token1}")
-
-
-class InvalidFee(TypeError):
-    """Raised when fee doesn't represent one of supported levels: 0.05%, 0.3%, 1%"""
-
-    def __init__(self, fee: Any):
-        super().__init__(f"Router V3 only supports three levels of fees: 0.05%, 0.3%, 1%, that equal 500,"
-                         f"3000 and 10000 respectively. You provided value - {fee}")
